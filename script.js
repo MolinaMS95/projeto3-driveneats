8460e55f6993ff345ecf4969a6c17f436a7f0bf9
Matheus Molina <matheusmolina95@gmail.com>
2022-08-12 15:51:02 -0300

    Terceira versão - botão ativado

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+let marcador = 0;
 function selecionar(item){
+    let condicao = false;
     const secao = item.parentNode;
     for (let i = 0; i <secao.children.length; i++){
         if(secao.children[i].classList.contains('selecionado')){
             secao.children[i].classList.remove('selecionado');
             secao.children[i].lastElementChild.classList.add('escondido');
+            condicao = true;
         }
+    }
+    if (condicao === false){
+        marcador = marcador + 1;
     }
     item.classList.add('selecionado');
     item.lastElementChild.classList.remove('escondido');
+    if(marcador === 3){
+        const botao = document.querySelector('.barraInferior button')
+        botao.classList.add('ativo');
+        botao.innerHTML = 'Fechar Pedido';
+    }
 }
